bc24dcd30855650ae7a867ece5796a24ea78a5b3
Elijhah Jones <elijhahdjones@gmail.com>
2023-05-17 10:04:29 -0400

    Add project files.

---
+++ Jonesify/MVM/View Model/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Jonesify.MVM.Model;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jonesify.MVM.View_Model
+{ 
+   
+  
+    internal class MainViewModel
+    {
+       public ObservableCollection<Item> Songs { get; set; }
+        public MainViewModel()
+        {
+            Songs = new ObservableCollection<Item>();
+        }
+
+        void PopulateCollection()
+        {
+            var client = new RestClient();
+            client.Authenticator = new OAuth2AuthorizationRequestHeaderAuthenticator("");
+        }
+    }
+}
